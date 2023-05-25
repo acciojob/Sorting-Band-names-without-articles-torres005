ae2d43a7fb95b64958daf330a0333996918aac66
torres005 <109883423+torres005@users.noreply.github.com>
2023-05-26 02:41:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+let bandName = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function bands(bde){
+	return bde.replace(/^(a|an|the)/i,'').trim();
+}
+
+const band = bandName.sort((a,b)=> bands(a) > bands(b) ? 1:-1 );
+
+console.log(band)
