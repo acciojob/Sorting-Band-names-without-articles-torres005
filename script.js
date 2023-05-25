febe1cba982b0159db8389399e07f8ae65c4ca05
torres005 <109883423+torres005@users.noreply.github.com>
2023-05-26 05:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 //your code here
-let bandName = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let band = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 function bands(bde){
 	return bde.replace(/^(a|an|the)/i,'').trim();
 }
 
-const band = bandName.sort((a,b)=> bands(a) > bands(b) ? 1:-1 );
+const bandName = band.sort((a,b)=> bands(a) > bands(b) ? 1:-1 );
+
+let roam = document.getElementById('band');
+roam.innerHTML = bandName.map(band => `<li>${band}</li>`);
+
+
 
 
